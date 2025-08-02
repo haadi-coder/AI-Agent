a423d0172881335c055639fb60a24e771cca9b12
abdugaffor2004 <abdughaffar7104@gmail.com>
2025-08-02 16:38:32 +0300

    feat: add agent and readfile tool

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/haadi-coder/AI-Agent
 
 go 1.24.4
+
+require (
+	github.com/anthropics/anthropic-sdk-go v1.6.2
+	github.com/haadi-coder/color v0.0.0-20250715144945-4377a95e2f6b
+	github.com/invopop/jsonschema v0.13.0
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/term v0.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,151 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/haadi-coder/color"
+)
 
 func main() {
-	fmt.Print("Hello")
+	client := anthropic.NewClient()
+	scanner := bufio.NewScanner(os.Stdin)
+	tools := []ToolDefinition{ReadFileDefinition}
+
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+
+		return scanner.Text(), true
+	}
+
+	agent := NewAgent(&client, getUserMessage, tools)
+	err := agent.Run(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+}
+
+type Agent struct {
+	client         *anthropic.Client
+	getUserMessage func() (string, bool)
+	tools          []ToolDefinition
+}
+
+type ToolDefinition struct {
+	Name        string                         `json:"name"`
+	Description string                         `json:"description"`
+	InputSchema anthropic.ToolInputSchemaParam `json:"input_schema"`
+	Function    func(input json.RawMessage) (string, error)
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool), tools []ToolDefinition) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+		tools:          tools,
+	}
+}
+
+func (a *Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MessageParam{}
+
+	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
+
+	readUserInput := true
+	for {
+		if readUserInput {
+			fmt.Print(color.Blue("You: "))
+
+			userInput, ok := a.getUserMessage()
+			if !ok {
+				break
+			}
+
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
+		}
+
+		outputMesg, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, outputMesg.ToParam())
+
+		toolResults := []anthropic.ContentBlockParamUnion{}
+		for _, content := range outputMesg.Content {
+			switch content.Type {
+			case "text":
+				fmt.Println(color.Yellow("Claude:"), content.Text)
+
+			case "tool_use":
+				result := a.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, result)
+			}
+		}
+
+		if len(toolResults) == 0 {
+			readUserInput = true
+			continue
+		}
+
+		readUserInput = false
+		conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
+	}
+
+	return nil
+}
+
+func (a *Agent) runInference(ctx context.Context, conv []anthropic.MessageParam) (*anthropic.Message, error) {
+	anthropicTools := make([]anthropic.ToolUnionParam, 0, len(a.tools))
+	for _, tool := range a.tools {
+		anthropicTools = append(anthropicTools, anthropic.ToolUnionParam{
+			OfTool: &anthropic.ToolParam{
+				Name:        tool.Name,
+				Description: anthropic.String(tool.Description),
+				InputSchema: tool.InputSchema,
+			},
+		})
+	}
+
+	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
+		Model:     anthropic.ModelClaude3_5Sonnet20241022,
+		MaxTokens: 1024,
+		Messages:  conv,
+		Tools:     anthropicTools,
+	})
+
+	return message, err
+}
+
+func (a *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			found = true
+			toolDef = tool
+			break
+		}
+	}
+
+	if !found {
+		return anthropic.NewToolResultBlock(id, "tool not found", true)
+	}
+
+	fmt.Print(color.Green("tool: "))
+	fmt.Printf("%s(%s)\n", name, input)
+
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+
+	return anthropic.NewToolResultBlock(id, response, false)
 }
